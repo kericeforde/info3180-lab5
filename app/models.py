fff32eb44abb03375c7a279f67e14ba13e67ede9
Kerice Forde <fordekerice@gmail.com>
2025-04-02 18:28:08 -0500

    Add api,model,migration

--- app/models.py
+++ app/models.py
@@ -1,1 +1,21 @@
-# Add any model classes for Flask-SQLAlchemy here
+from app import db
+
+
+class Movie(db.Model):
+    #Table name
+    __tablename__= "movies"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    description = db.Column(db.String(255))
+    poster =db.Column(db.String(80))
+    created_at =db.Column(db.Datetime)
+
+    def __init__(self, title,description,poster):
+        self.title=title
+        self.description=description
+        self.poster=poster
+
+
+
+
--- app/views.py
+++ app/views.py
@@ -5,9 +5,12 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
+from app import app,db
 from flask import render_template, request, jsonify, send_file
 import os
+from app.forms import MovieForm
+from app.models import  Movie
+from werkzeug.utils import secure_filename 
 
 
 ###
@@ -17,6 +20,35 @@ import os
 @app.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
+
+@app.route('/api/v1/movies',method=['POST'])
+def movies():
+    form=MovieForm()
+
+    if form.validate_on_submit():
+        
+        title=form.title.data
+        description=form.description.data
+        poster=form.poster.data
+
+        name = secure_filename(poster.filename)  # Secure the filename
+        path = os.path.join(app.config['UPLOAD_FOLDER'],name)  
+        poster.save(path)
+
+        movie=Movie(title,description,name)
+        db.session.add(movie)
+        db.session.commit()
+        return jsonify( { 
+            "message": "Movie Successfully added", 
+            "title": title,
+            "poster": name, 
+            "description": description}), 201
+    return jsonify({"errors": form_errors(form)}),400
+        
+
+
+
+
 
 
 ###
